c814534ea29a617629ef2d072bda1e4327c814dd
kinoshita-m <kinoshita-m@klab.com>
2018-08-09 15:39:21 +0900

    fix

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ func add(a, b int) int {
 	return a + b
 }
 
+func minus(a, b int) int {
+	return a - b
+}
+
 func main() {
 	fmt.Printf("1 + 2 = %d\n", add(1, 2))
+	fmt.Printf("1 - 2 = %d\n", minus(1, 2))
 }
